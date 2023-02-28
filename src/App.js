9cde4a78b3870534766ae18dde3a97798899e651
User <user@Users-MacBook-Pro.local>
2023-02-28 11:48:31 +0000

    added basics of todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { name: "Clean Car", done: false },
+      { name: "Clean House", done: false },
+      { name: "Buy milk", done: false },
+    ],
+  };
+
+  onInput = (e) => {
+    this.setState({ userInput: e.target.value });
+  };
+
+  onAdd = () => {
+    //check the user actually entered something
+    if (!this.state.userInput || this.state.userInput.length < 4) {
+      return;
+    }
+
+    //defensive check, if the item already exists, ignore it
+    const result = this.state.todos.some((item) => {
+      return item.name === this.state.userInput;
+    });
+
+    //if the item was found, skip it
+    if (result) {
+      return;
+    }
+
+    //copy user input unto the todos array
+    const todo = { done: false, name: this.state.userInput };
+    this.setState({ todos: [...this.state.todos, todo] });
+  };
+
+  onDelete = (name) => {
+    //remove and item
+
+    const index = this.state.todos.findIndex((todo) => todo.name === name);
+
+    const todos = [...this.state.todos];
+    todos.splice(index, 1);
+
+    this.setState({ todos });
+  };
+
+  onDoneToggle = (name) => {
+    const index = this.state.todos.findIndex((todo) => todo.name === name);
+
+    const todos = [...this.state.todos];
+
+    todos[index].done = !todos[index].done;
+
+    this.setState({ todos });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <>
+        {/* user input controls */}
+        <div className="controls">
+          <input onInput={this.onInput} type="text" />
+          <button onClick={this.onAdd}>Add</button>
+        </div>
+
+        {/* below is the code that creates the todo display */}
+        {this.state.todos.map((todo) => (
+          <div className={todo.done ? "done todoItem" : "undone todoItem"}>
+            <p>{todo.name}</p>
+            <button onClick={() => this.onDoneToggle(todo.name)}>
+              {todo.done ? "Undo" : "Do"}
+            </button>
+            <button onClick={() => this.onDelete(todo.name)}>Delete</button>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
 export default App;
